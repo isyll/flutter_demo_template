72091b04337e574802258ec39e366b9fa6d6e238
Ibrahima Sylla <isyll.net@gmail.com>
2025-01-22 10:18:42 +0000

    First commit

---
+++ android/app/src/main/kotlin/com/isyll/flutter_demo_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.isyll.flutter_demo_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_template/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Future<void> main() async {
+  runApp(const ProviderScope(child: App()));
+}
